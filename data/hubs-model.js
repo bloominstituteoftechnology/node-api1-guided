1d9de136e2ceaec2c3b30d37916bc9569fcd9016
Diandra Ryan-Mas <dryanmas@Diandras-Air.hitronhub.home>
2019-05-09 10:10:07 -0700

    refactored helpers to knex

---
+++ data/hubs-model.js
@@ -0,0 +1,47 @@
+const knex = require('knex');
+const config = require('../knexfile.js');
+const db = knex(config.development);
+
+module.exports = {
+  find,
+  findById,
+  add,
+  remove,
+  update
+};
+
+function find(query) {
+  const { page = 1, limit = 2, sortby = 'id', sortdir = 'asc' } = query;
+  const offset = limit * (page - 1);
+
+  let rows = db('hubs')
+    .orderBy(sortby, sortdir)
+    .limit(limit)
+    .offset(offset);
+
+  return rows;
+}
+
+function findById(id) {
+  return db('hubs')
+    .where({ id })
+    .first();
+}
+
+async function add(hub) {
+  const [id] = await db('hubs').insert(hub);
+
+  return findById(id);
+}
+
+function remove(id) {
+  return db('hubs')
+    .where({ id })
+    .del();
+}
+
+function update(id, changes) {
+  return db('hubs')
+    .where({ id })
+    .update(changes, '*');
+}
