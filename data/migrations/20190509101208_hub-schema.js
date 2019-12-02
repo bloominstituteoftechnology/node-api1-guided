33777a47cdbb11895fe32d5788738302d32b6d12
Luis Hernandez <luishrd@gmail.com>
2019-12-02 08:24:45 -0700

    simplifies migrations and seeds code

--- data/migrations/20190509101208_hub-schema.js
+++ data/migrations/20190509101208_hub-schema.js
@@ -1,13 +1,16 @@
+exports.up = function(knex) {
+  return knex.schema.createTable("hubs", tbl => {
+    tbl.increments();
 
-exports.up = function(knex, Promise) {
-  return knex.schema
-    .createTable('hubs', tbl => {
-      tbl.increments();
-      tbl.string('name').notNullable().unique();
-      tbl.timestamps(true, true);
-    })
+    tbl
+      .string("name")
+      .notNullable()
+      .unique();
+
+    tbl.timestamps(true, true);
+  });
 };
 
-exports.down = function(knex, Promise) {
-  return knex.schema.dropTableIfExists('hubs');
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists("hubs");
 };
--- data/seeds/01-hubs.js
+++ data/seeds/01-hubs.js
@@ -1,14 +1,14 @@
+exports.seed = function(knex) {
+  return knex("hubs")
+    .truncate()
+    .then(function() {
+      const hubs = [
+        { name: "api-1" }, // 1
+        { name: "api-2" }, // 2
+        { name: "api-3" }, // 3
+        { name: "api-4" } // 4
+      ];
 
-exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
-  return knex('hubs').truncate()
-    .then(function () {
-      // Inserts seed entries
-      return knex('hubs').insert([
-        { name: 'api-1' }, // 1
-        { name: 'api-2' }, // 2
-        { name: 'api-3' }, // 3
-        { name: 'api-4' }, // 4
-      ]);
+      return knex("hubs").insert(hubs);
     });
 };
