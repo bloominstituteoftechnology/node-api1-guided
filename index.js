a75ba6731d063abbbbd930a9f2b85b8241fe2aa5
Brandon-Harris <brandonharris177@gmail.com>
2020-03-09 11:34:38 -0600

    added endpoints

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const express = require("express");
 
 const server = express();
 
+server.get('/', (req, res) => {
+    res.status(200).json({api: "running"})
+})
+
+server.get('/hello', (req, res) => {
+    res.status(200).json({hello: "Web 27"})
+})
+
 const PORT = 5000;
 
-server.listen(PORT, () => console.log(`\n *** API on http://localhost:${PORT}***\n`))
+server.listen(PORT, () => console.log(`\n *** API on http://localhost:${PORT} ***\n`))
