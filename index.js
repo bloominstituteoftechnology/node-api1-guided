0f39c61fc6025e0e72160e97e667fc09f9e60829
Brandon-Harris <brandonharris177@gmail.com>
2020-03-10 08:42:03 -0600

    finished

--- index.js
+++ index.js
@@ -1,14 +1,38 @@
 const express = require("express");
+const shortid =  require("shortid");
 
 const server = express();
 
+server.use(express.json());
+
+let hubs = []
+let lessons = []
+
 server.get('/', (req, res) => {
     res.status(200).json({api: "running"})
-})
+});
 
 server.get('/hello', (req, res) => {
     res.status(200).json({hello: "Web 27"})
+});
+
+server.get('/api/lessons', (req, res) => {
+    res.status(200).json(lessons)
 })
+
+server.post('/api/hubs', (req, res) => {
+    const hubInfo = req.body;
+    hubInfo.id = shortid.generate();
+    hubs.push(hubInfo);
+    res.status(201).json(hubInfo);
+});
+
+server.post('/api/lessons', (req, res) => {
+    const lessonInfo = req.body;
+    lessonInfo.id = shortid.generate();
+    lessons.push(lessonInfo);
+    res.status(201).json(lessonInfo);
+});
 
 const PORT = 5000;
 
