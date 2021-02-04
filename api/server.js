f075549fd1c8e643351692fdbabaaef8d269b765
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-02-04 15:56:45 +0100

    WIP

--- api/server.js
+++ api/server.js
@@ -1,1 +1,31 @@
-// write the server here and export it
+// IMPORTS AT THE TOP
+// IMPORTS AT THE TOP
+// IMPORTS AT THE TOP
+
+// INSTANCE OF EXPRESS APP
+// INSTANCE OF EXPRESS APP
+// INSTANCE OF EXPRESS APP
+
+// GLOBAL MIDDLEWARE
+// GLOBAL MIDDLEWARE
+// GLOBAL MIDDLEWARE
+
+// ENDPOINTS
+// ENDPOINTS
+// ENDPOINTS
+
+// [GET] / (Hello World endpoint)
+
+// [GET] /api/dogs/:id (Read of CRUD, fetch dog by :id)
+
+// [GET] /api/dogs (Read of CRUD, fetch all dogs)
+
+// [POST] /api/dogs (C of CRUD, create new dog from JSON payload)
+
+// [PUT] /api/dogs/:id (U of CRUD, update dog with :id using JSON payload)
+
+// [DELETE] /api/dogs/:id (D of CRUD, remove dog with :id)
+
+// EXPOSING THE SERVER TO OTHER MODULES
+// EXPOSING THE SERVER TO OTHER MODULES
+// EXPOSING THE SERVER TO OTHER MODULES
