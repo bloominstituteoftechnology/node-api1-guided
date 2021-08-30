b0a6c2dc6d190ecefb4468f840f49edab8ef21af
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-08-30 19:14:53 +0200

    wip

--- index.js
+++ index.js
@@ -1,16 +1,4 @@
-// import the server and start it
-const express = require('express') // import express from 'express' // ES6 modules
-
-const server = express() // instance of an express app
-
-server.get('/hello', (req, res) => {
-  // pull info of interest out of the req
-  res.json({ message: 'web 45 rocks'}) // sending back a response
-})
-
-server.use('*', (req, res) => { // catch all, put it at the end of your endpoints
-  res.status(404).json({ message: 'sorry, not found!'})
-})
+const server = require('')
 
 server.listen(5000, () => { // only one program can hog 5000 at a time
   console.log('listening on port 5000') // success callback
