90dc9ee4717c155e597c721c2b6e62cc64f227ed
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-08-30 18:44:14 +0200

    wip

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 // import the server and start it
 const express = require('express') // import express from 'express' // ES6 modules
 
+const server = express() // instance of an express app
+
+server.get()
