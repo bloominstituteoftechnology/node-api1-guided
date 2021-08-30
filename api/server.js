8af9d33133055b785d178756477c53bd093a9253
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-08-30 19:27:39 +0200

    wip

--- api/server.js
+++ api/server.js
@@ -15,6 +15,9 @@ server.get('/api/dogs/:id', (req, res) => {
   res.json('fetch dog by its id')
 })
 // [GET] /api/dogs (R of CRUD, fetch all dogs)
+server.get('/api/dogs', (req, res) => {
+  res.json('')
+})
 // [POST] /api/dogs (C of CRUD, create new dog from JSON payload)
 // [PUT] /api/dogs/:id (U of CRUD, update dog with :id using JSON payload)
 // [DELETE] /api/dogs/:id (D of CRUD, remove dog with :id)
