af40fefa7d4338ee1bb86cb1bf6ac8219983af45
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-08-30 19:15:11 +0200

    wip

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const server = require('')
+const server = require('./api/server.js')
 
 server.listen(5000, () => { // only one program can hog 5000 at a time
   console.log('listening on port 5000') // success callback
