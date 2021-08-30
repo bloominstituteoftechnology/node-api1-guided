3f4b8cf5baf1299addebbaca4c6bae1714f0021a
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-08-30 19:20:28 +0200

    wip

--- api/server.js
+++ api/server.js
@@ -1,12 +1,15 @@
 // IMPORTS AT THE TOP
 const express = require('express')
 // INSTANCE OF EXPRESS APP
-
+const server = express()
 // GLOBAL MIDDLEWARE
 
 // ENDPOINTS
 
 // [GET] / (Hello World endpoint)
+server.get('/', (req, res) => {
+  res.st
+})
 // [GET] /api/dogs/:id (R of CRUD, fetch dog by :id)
 // [GET] /api/dogs (R of CRUD, fetch all dogs)
 // [POST] /api/dogs (C of CRUD, create new dog from JSON payload)
