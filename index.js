5532cb0cc56b6c82e37c1d46a4a77ac22154f5f2
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-08-30 18:52:36 +0200

    wip

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ server.get('/hello', (req, res) => {
   res.json({ message: 'web 45 rocks'}) // sending back a response
 })
 
-server.listen(5000, () => {
-  console.log('listening on port 5000')
+server.listen(5000, () => { // only one program can hog 5000 at a time
+  console.log('listening on port 5000') // success callback
 })
