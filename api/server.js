90456143c31339467b30bc7eeaa77fc44172fced
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-09-30 17:01:16 +0200

    comment

--- api/server.js
+++ api/server.js
@@ -7,8 +7,8 @@
 // ENDPOINTS
 
 // [GET] / (Hello World endpoint)
-// [GET] /api/dogs/:id (R of CRUD, fetch dog by :id)
 // [GET] /api/dogs (R of CRUD, fetch all dogs)
+// [GET] /api/dogs/:id (R of CRUD, fetch dog by :id)
 // [POST] /api/dogs (C of CRUD, create new dog from JSON payload)
 // [PUT] /api/dogs/:id (U of CRUD, update dog with :id using JSON payload)
 // [DELETE] /api/dogs/:id (D of CRUD, remove dog with :id)
