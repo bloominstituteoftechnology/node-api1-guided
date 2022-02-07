c75317c9df770d44a272b9069276899b4e2af8e4
James Blacklock <jamesblacklock@protonmail.com>
2022-02-07 14:01:46 -0500

    committing to "web50-jamesblacklock"

--- api/server.js
+++ api/server.js
@@ -78,6 +78,22 @@ server.put('/api/dogs/:id', async (req, res) => {
     }
 });
 
+server.delete('/api/dogs/:id', (req, res) => {
+    let { id } = req.params;
+    dogModel.delete(id)
+        .then(dog => {
+            if(dog == null) {
+                res.status(404).json({ message: `dog ${id} not found!` });
+                return;
+            }
+
+            res.status(200).json(dog);
+        })
+        .catch(() => {
+            res.status(500).json({ message: `could not delete dog!` });
+        });
+});
+
 // [DELETE] /api/dogs/:id (D of CRUD, remove dog with :id)
 
 module.exports = server;
