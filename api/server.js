31a1783e83f89ae1f746654d6eb4052330a965b2
James Blacklock <jamesblacklock@protonmail.com>
2022-02-07 12:53:01 -0500

    committing to "web50-jamesblacklock"

--- api/server.js
+++ api/server.js
@@ -10,10 +10,16 @@ server.get('/', (req, res) => {
 });
 
 server.get('/api/dogs', (req, res) => {
-    dogModel.findAll().then(dogs => {
-        res.json(dogs);
-    });
+    dogModel.findAll()
+        .then(dogs => {
+            throw new Error();
+            // res.json(dogs);
+        })
+        .catch(() => {
+            res.status(500).json({ message: "could not get the dogs!" });
+        });
 });
+
 // [GET]    /api/dogs     (R of CRUD, fetch all dogs)
 // [GET]    /api/dogs/:id (R of CRUD, fetch dog by :id)
 // [POST]   /api/dogs     (C of CRUD, create new dog from JSON payload)
