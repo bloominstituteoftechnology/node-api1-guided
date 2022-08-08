82263e6aeb33b1e3cd7c5c917e964582e241f86d
James Blacklock <jamesblacklock@protonmail.com>
2022-08-08 13:45:47 -0400

    committing to "lecture"

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-// const server = require('./api/server');
+const server = require('./api/server');
 
-// server.listen(9000, () => {
-//     console.log('server is now listening on port 9000!');
-// });
-
-
-
-console.log(100);
+server.listen(9000, () => {
+    console.log('server is now listening on port 9000!');
+});
