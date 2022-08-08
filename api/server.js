fbf9962e1df2a62e7b5c95d8d5233e019f3656fa
James Blacklock <jamesblacklock@protonmail.com>
2022-08-08 13:27:16 -0400

    committing to "lecture"

--- api/server.js
+++ api/server.js
@@ -17,10 +17,20 @@ server.get('/', (req, res) => {
 });
 
 // [GET]    /api/dogs     (R of CRUD, fetch all dogs)
+server.VERB('/api/dogs', (req, res) => {});
+
 // [GET]    /api/dogs/:id (R of CRUD, fetch dog by :id)
+server.VERB('/api/dogs/:id', (req, res) => {});
+
 // [POST]   /api/dogs     (C of CRUD, create new dog from JSON payload)
+server.VERB('/api/dogs', (req, res) => {});
+
 // [PUT]    /api/dogs/:id (U of CRUD, update dog with :id using JSON payload)
+server.VERB('/api/dogs/:id', (req, res) => {});
+
 // [DELETE] /api/dogs/:id (D of CRUD, remove dog with :id)
+server.VERB('/api/dogs/:id', (req, res) => {});
+
 
 // EXPOSING THE SERVER TO OTHER MODULES
 module.exports = server
