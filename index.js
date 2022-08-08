414fdea01d1a1d90652b7137e17ff83fa9c24bd3
James Blacklock <jamesblacklock@protonmail.com>
2022-08-08 12:51:28 -0400

    committing to "lecture"

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-// import the server and start it!
-console.log('my first node application');
+// import express from 'express';
+const express = require('express');
+
+const server = express();
+
+server.listen(9000, () => {
+    console.log('server is now listening on port 9000!');
+});
