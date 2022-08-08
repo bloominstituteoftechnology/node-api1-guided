c5c0045fe496189f385a8379c852296f971de6f7
James Blacklock <jamesblacklock@protonmail.com>
2022-08-08 12:57:26 -0400

    committing to "lecture"

--- index.js
+++ index.js
@@ -6,3 +6,8 @@ const server = express();
 server.listen(9000, () => {
     console.log('server is now listening on port 9000!');
 });
+
+
+server.get('/test1', (req, res) => {
+    res.send('test1!');
+})
